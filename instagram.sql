7512c8cd135e36beda3a6481f928771a8c0faaf3
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-04-22 12:34:03 +0000

    creatiing table

---
+++ instagram.sql
@@ -0,0 +1,18 @@
+create database instagram;
+
+use instagram;
+
+create table instagram (
+name varchar (50),
+email varchar(50),
+followers int,
+following int
+);
+
+
+insert into instagram values 
+('Satyam Gupta', 'Satu@gmail.com',168,253),
+('Sujal Gupta','Sujla@gmail.com',185,166),
+('aaadmi gupta','aadmi@gmail.com',189,250),
+('sejal Gupta','sejalji@gmail.com',160,323);
+
